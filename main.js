a1051eb6c042a10a48613c174963dc61738d4e54
orzcode <71000311+orzcode@users.noreply.github.com>
2024-07-23 17:21:33 +1200

    Initial commit

---
+++ main.js
@@ -0,0 +1,12 @@
+import './style.css'
+import javascriptLogo from './javascript.svg'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.js'
+/* ----------  DEFAULT PATHS  ------------ */
+// 2 pics
+
+// top pic: box shape, face,
+// full size, med size, and mobile ver (portrait).
+
+// bottom pic: wide shape, action image,
+// full size, tablet ver (portrait), mobile ver (portrait, of tablet ver)
